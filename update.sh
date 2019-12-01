2b90b9ad4c9d05273c8262aff371a2ef2ac9bd8c
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-01 19:08:00 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@
 # Mod from MrDoob for PTS
 # GNU:        General Public License v3.0
 ################################################################################
+
 sudocheck() {
   if [[ $EUID -ne 0 ]]; then
     tee <<-EOF
@@ -19,8 +20,13 @@ EOF
 }
 
 base() {
-rm -rf /opt/pgstage && mkdir -p /opt/pgstage
-ansible-playbook /opt/ptsupdate/stage/pgstage.yml
+rm -rf /opt/ptsupdate
+mkdir -p /opt/ptsupdate 1>/dev/null 2>&1
+git clone --single-branch https://github.com/PTS-Team/PTS-Update.git /opt/ptsupdate 1>/dev/null 2>&1
+chown -cR 100:1000 /opt/ptsupdate 1>/dev/null 2>&1 
+chmod -cR 775 /opt/ptsupdate 1>/dev/null 2>&1
+rm -rf /opt/pgstage && mkdir -p /opt/pgstage 1>/dev/null 2>&1
+ansible-playbook /opt/ptsupdate/stage/pgstage.yml 1>/dev/null 2>&1
 }
 
 mainstart() {
