8b8759c4086809e7eab80b8af659c188ce14bb49
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-01 17:48:14 +0100

    Update file.sh

--- version/file.sh
+++ version/file.sh
@@ -5,8 +5,8 @@
 # Mod from MrDoob for PTS
 # GNU:        General Public License v3.0
 ################################################################################
-source /opt/plexguide/menu/functions/functions.sh
-source /opt/plexguide/menu/functions/install.sh
+source /opt/ptsupdate/functions/functions.sh
+source /opt/ptsupdate/functions/install.sh
 
 sudocheck() {
   if [[ $EUID -ne 0 ]]; then
@@ -24,7 +24,7 @@ EOF
 downloadpg() {
   rm -rf /opt/plexguide
   git clone --single-branch https://github.com/PTS-Team/PTS-Team.git /opt/plexguide  1>/dev/null 2>&1
-  ansible-playbook /opt/plexguide/menu/version/missing_pull.yml
+  ansible-playbook /opt/ptsupdate/version/missing_pull.yml
   ansible-playbook /opt/plexguide/menu/alias/alias.yml  1>/dev/null 2>&1
   rm -rf /opt/plexguide/place.holder >/dev/null 2>&1
   rm -rf /opt/plexguide/.git* >/dev/null 2>&1
