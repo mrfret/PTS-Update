b362360118d0aa376a34b6a05d4397678b29d9c4
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-10 19:31:34 +0100

    update autopart

---
+++ autoupdate/update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Title:      PTS major file
+# org.Author(s):  Admin9705 - Deiteq
+# Mod from MrDoob for PTS
+# GNU:        General Public License v3.0
+################################################################################
+
+start0() {
+mainstart
+alias
+owned
+check
+}
+
+mainstart() {
+  file="/opt/pgstage/place.holder"
+  waitvar=0
+  while [ "$waitvar" == "0" ]; do
+    sleep .5
+    if [ -e "$file" ]; then waitvar=1; fi
+  done
+   ansible-playbook /opt/ptsupdate/autoupdate/version/choice.yml
+}
+
+########end funtions // execute commands
+alias() {
+  ansible-playbook /opt/plexguide/menu/alias/alias.yml
+}
+
+owned() {
+  chown -cR 1000:1000 /opt/plexguide
+  chmod -R 775 /opt/plexguide
+}
+
+check() {
+file="/opt/plexguide/menu/pg.yml"
+  if [[ -f $file ]]; then
+  tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅ All files Valid and latest version pulled
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+ else ansible-playbook /opt/plexguide/menu/version/missing_pull.yml; fi
+}
